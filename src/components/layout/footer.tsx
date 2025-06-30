2669a40b0b11ead0f5385718f7ff40a67dec0ce5
Rohan Patra <rohanpatra@berkeley.edu>
2025-06-29 17:53:23 -0700

    Update footer copyright year to 2025
    Changed the copyright year in the footer from 2024 to 2025.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="border-t p-4 text-center">
-      <p>&copy; 2024 Arbitron. All rights reserved.</p>
+      <p>&copy; 2025 Arbitron. All rights reserved.</p>
     </footer>
   );
 }
